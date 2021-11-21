b6952904b02be5b5fd270b789b1c3f0d494a535b
Aitsam Ahad <aitsama.ahad@gmail.com>
2021-11-21 15:20:19 +0500

    Added DIvider and link to portoflio page.

--- src/templates/Work.jsx
+++ src/templates/Work.jsx
@@ -3,29 +3,46 @@ import Heading from "@common/Heading"
 import Grid from "@mui/material/Grid"
 import React from "react"
 import { PortfolioData } from "src/portfolio"
-import styled from "styled-components"
+import Divider from "@mui/material/Divider"
+import { useRouter } from "next/router"
 
 function Work() {
+  const history = useRouter()
+  const FiveRecentPortfolio = PortfolioData?.slice(0, 5) ?? []
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} textAlign='center'>
         <Heading>
-          Work <br /> <small>Some Things I've Built</small>
+          Work <br />{" "}
+          <small>
+            Some Things I've Built{" "}
+            <span
+              style={{ cursor: "pointer" }}
+              onClick={() => history.push("/portfolio")}
+            >
+              <u>(See All)</u>
+            </span>
+          </small>
         </Heading>
       </Grid>
       <Grid item xs={12}>
         <Grid container spacing={2}>
-          {PortfolioData?.map((data, index) => (
+          {FiveRecentPortfolio?.map((data, index) => (
             <Grid item xs={4} key={index}>
               <PortfolioCard
                 image={data?.image}
                 title={data?.title}
                 subtitle={data?.subtitle}
                 link={data?.link}
+                tech={data?.tech}
               />
             </Grid>
           ))}
         </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Divider variant='middle' />
       </Grid>
     </Grid>
   )
