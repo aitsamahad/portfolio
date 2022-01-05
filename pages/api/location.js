681f5065f600daa0e0cbcdf235085485a50906de
Aitsam Ahad <aitsam.ahad@gmail.com>
2022-01-05 14:44:33 +0500

    Added resource for location api call.

--- pages/api/location.js
+++ pages/api/location.js
@@ -1,13 +1,23 @@
-import data from "../../data.json"
+import jsonData from "../../data.json"
+import axios from "axios"
 
-export default function handler(req, res) {
+axios.defaults.headers.common["uid"] = `f43d931d1eee42f1b84036006918e80f`
+
+export default async function handler(req, res) {
 	console.log(req.query)
 	const { search } = req?.query
 
-	const removeDuplicates = data?.filter(
+	const removeDuplicates = jsonData?.filter(
 		(value, index, self) => index === self.findIndex((t) => t.id === value.id && t.lat === value.lat)
 	)
 
 	let filtered = removeDuplicates?.filter((x) => x.formatted?.toLowerCase()?.includes(search?.toLowerCase()))
-	res.status(200).json({ data: filtered })
+
+	const { status, data } = await axios.get(
+		`https://dispatch-backend-v2.herokuapp.com/api/icabbi/search-address/?query=${search?.toLowerCase()}`
+	)
+
+	let response = data ?? filtered
+
+	res.status(200).json({ data: response })
 }
