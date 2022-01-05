a9b504deb145c159d5f071e4e8b1591514fbd5b1
Aitsam Ahad <aitsam.ahad@gmail.com>
2022-01-05 16:03:25 +0500

    Added cors middleware.

--- pages/api/location.js
+++ pages/api/location.js
@@ -1,11 +1,28 @@
 import jsonData from "../../data.json"
 import axios from "axios"
-
+import Cors from "cors"
 axios.defaults.headers.common["uid"] = `f43d931d1eee42f1b84036006918e80f`
 
+const cors = Cors({
+	methods: ["GET", "HEAD"],
+})
+
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result)
+			}
+
+			return resolve(result)
+		})
+	})
+}
+
 export default async function handler(req, res) {
+	await runMiddleware(req, res, cors)
 	console.log(req.query)
-	res.header("Access-Control-Allow-Origin", "*")
+
 	const { search } = req?.query
 
 	const removeDuplicates = jsonData?.filter(
